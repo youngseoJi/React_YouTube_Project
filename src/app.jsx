589685dc8762d52d5c21a2e703d6ef284650649e
youngseoJi <estell.ys.j@gmail.com>
2022-02-27 22:06:17 +0900

    back to the list when searching

--- src/app.jsx
+++ src/app.jsx
@@ -8,19 +8,25 @@ import VideoDetail from "./components/video_detail/video_detail";
 
 function App({ youtube }) {
   // 컴포넌트가 마운트(사용자에세 보여질때)가 되었을때 유튜브로 부터 데이터를 받아오는 부분 작성
+
   const [videos, setVideos] = useState([]);
-  // 선택한 비디오를 보여주기 위한, 초기상태 null
-  // 선택하면 해당 비디오를 보여준다.
+  // 선택한 비디오를 보여주기 위한, 초기상태 null / 선택하면 해당 비디오를 보여준다.
   const [selectedVideo, setSelectedVideo] = useState(null);
+  // 검색중일때 띄울 화면
+  // const [isLoding, setIsLoding] = useState(false);
+
   // 선택한 비디오로 변경한다.
   const selectVideo = (video) => {
     setSelectedVideo(video);
   };
 
   const search = (query) => {
-    youtube
-      .search(query) //
-      .then((videos) => setVideos(videos));
+    // 처음엔 항상 초기화 시키기 - 기본브라우저화면
+    setSelectedVideo(null);
+
+    youtube.search(query).then((videos) => {
+      setVideos(videos);
+    });
   };
   // 컴포넌트가 마운트가 되었거나 업데이트 될때마다 호출되는 함수  useEffect
   useEffect(() => {
@@ -36,6 +42,7 @@ function App({ youtube }) {
       {/* 검색 구현 컴포넌트에 search 함수전달 */}
       {/* 선택된 비디오가 있으면 VideoDetail 컴포넌트를 보이게 한다. (컴포넌트에 선택된 비디오를 전달한다.)*/}
       <SearchHeader onSearch={search} />
+
       <section className={styles.content}>
         {selectedVideo && (
           <div className={styles.detail}>
